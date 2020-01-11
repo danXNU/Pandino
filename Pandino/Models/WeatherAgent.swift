4e5016f33183db364202ca59f5f049767ed9a0ba
DaniTox <danitox@protonmail.com>
2020-01-11 21:47:03 +0100

    fixed some weather api failures

--- Pandino/Models/WeatherAgent.swift
+++ Pandino/Models/WeatherAgent.swift
@@ -85,21 +85,21 @@ class WeatherAgent: ObservableObject {
 }
 
 struct CurrentLocalWeather: Decodable {
-    let base: String
+    let base: String?
     let clouds: Clouds
-    let cod: Int
+    let cod: Int?
     let coord: Coord
-    let dt: Int
-    let id: Int
+    let dt: Int?
+    let id: Int?
     let main: Main
-    let name: String
+    let name: String?
     let sys: Sys?
-    let visibility: Int
+    let visibility: Int?
     let weather: [Weather]
     let wind: Wind
 }
 struct Clouds: Decodable {
-    let all: Int
+    let all: Int?
 }
 struct Coord: Decodable {
     let lat: Double
@@ -127,20 +127,20 @@ struct Main: Decodable {
     }
 }
 struct Sys: Decodable {
-    let country: String
-    let id: Int
+    let country: String?
+    let id: Int?
 //    let message: Double
     let sunrise: UInt64
     let sunset: UInt64
-    let type: Int
+    let type: Int?
 }
 struct Weather: Decodable {
-    let description: String
-    let icon: String
-    let id: Int
-    let main: String
+    let description: String?
+    let icon: String?
+    let id: Int?
+    let main: String?
 }
 struct Wind: Decodable {
 //    let deg: Int
-    let speed: Double
+    let speed: Double?
 }
